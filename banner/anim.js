d51ce474e14f0b902e17f64d484e248f3742abcd
Irina Chervyakova <irinachervaykova@gmail.com>
2019-03-31 00:38:28 +0200

    new commit

---
+++ banner/anim.js
@@ -0,0 +1,7 @@
+TweenMax.to(".iphones", 3, {x: 230});
+
+TweenMax.to(".logo", 1, {opacity: 1, delay: 2.5});
+
+TweenMax.to(".subtitle", 1, {opacity: 1,delay: 3});
+
+TweenMax.to(".button", 1, {opacity: 1, delay: 3.5});
